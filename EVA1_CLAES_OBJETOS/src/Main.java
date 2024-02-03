81f1e6cb1985264da3f306665e4b8e886e03f7ae
Noble0505 <143568699+Noble0505@users.noreply.github.com>
2024-02-02 18:50:12 -0600

    Add files via upload

---
+++ EVA1_CLAES_OBJETOS/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+       Persona perso1;//declaro un identificador (variable) de tipo persona
+        //solo he declarado la varianble, no he creado un objeto
+        //Crear el objeto (instanciar) consiste en asignar memoria.
+        int valor;
+        System.out.println();
+    }
+}
+//CLASE PERSONA
+/*
+ Nombre de de la clase --> Nomenclatura Camelcase
+ class Nombre de la Clase { cuerpo de la clase }
+*/
+class Persona{
+
+}
